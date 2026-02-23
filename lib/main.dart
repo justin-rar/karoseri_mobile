da98cb7d2656a5dab5e8b9a826711650f9f460b2
cleosaav <vania06putrii@gmail.com>
2026-02-23 13:07:56 +0700

    connectdatabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'features/dashboard/dashboard_page.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url:
+        'https://nsyttzfvlblsqmfzjyxf.supabase.co', // Sesuai gambar API URL kamu
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zeXR0emZ2bGJsc3FtZnpqeXhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4MTU4NjEsImV4cCI6MjA4NzM5MTg2MX0.fVfHyT3Xw4ioGrYmYS9mwB3zSuFxrdL8AdvIl0V3Cso',
+  );
+
   runApp(const KaroseriApp());
 }
 
@@ -13,6 +23,8 @@ class KaroseriApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Manajemen Karoseri',
+      //tema warna (opsional)
+      theme: ThemeData(primarySwatch: Colors.brown, useMaterial3: true),
       home: const DashboardPage(),
     );
   }
